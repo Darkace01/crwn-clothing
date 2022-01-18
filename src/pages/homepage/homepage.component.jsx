d9c243510e5162c67b0bdd1781024553b556d1d7
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-01-18 16:44:26 +0100

    Homepage and directory component fix

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//styling
+import "./homepage.styles.scss";
+
+//components
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
