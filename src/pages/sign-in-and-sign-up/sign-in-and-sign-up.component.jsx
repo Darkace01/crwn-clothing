9c29bcb55de28fe9bf4c87b336d49adb095edca5
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-01-19 13:44:55 +0100

    Sign In component

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,8 +3,13 @@ import React from "react";
 //styling
 import "./sign-in-and-sign-up.styles.scss";
 
+//components
+import SignIn from "../../components/sign-in/sign-in.component";
+
 const SignInAndSignUpPage = () => (
-  <div className="sign-in-and-sign-up">Sign In</div>
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
 );
 
 export default SignInAndSignUpPage;
