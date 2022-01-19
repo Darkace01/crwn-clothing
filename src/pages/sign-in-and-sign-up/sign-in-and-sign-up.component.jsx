6e221329cf4a687ae5d27baf378f2489adeba9c6
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-01-19 13:27:30 +0100

    sign-in-and-sign-up page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//styling
+import "./sign-in-and-sign-up.styles.scss";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">Sign In</div>
+);
+
+export default SignInAndSignUpPage;
