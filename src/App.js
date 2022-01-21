4c820dfa27f33d1330ce1c655631afab18a84f32
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-01-21 17:04:25 +0100

    Storing user data to firebase

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 //utils
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -21,8 +21,8 @@ class App extends React.Component {
   }
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+      createUserProfileDocument(user);
     });
   }
   componentWillUnmount() {
