1508abbfba2603c0591771715ff3c1e5613dbd00
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-01-24 16:53:01 +0100

    Get currentUser from redux in header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+//redux
+import { connect } from "react-redux";
+
 //styling
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
@@ -33,4 +36,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
