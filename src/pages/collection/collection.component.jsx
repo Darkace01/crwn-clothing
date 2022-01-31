029c736522db59c9ce7c5371597218174f92b040
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-01-31 10:23:52 +0100

    fetch data from props

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { connect } from "react-redux";
 //components
 import CollectionItem from "../../components/collection-item/collection-item.component";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 //styling
 import "./collection.styles.scss";
@@ -14,4 +15,8 @@ const CollectionPage = ({ match }) => (
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
