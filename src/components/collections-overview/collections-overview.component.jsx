b9f6ffa20d49df3e5124797d3e61039622743f3d
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-01-31 15:53:59 +0100

    Convevrting objects to arrays of keys

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 //utils
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
 //components
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
@@ -20,7 +20,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
