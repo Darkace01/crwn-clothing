56f469881b7bb91697d9e08e7906d369483e8479
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-01-31 14:10:49 +0100

    Shop Collections

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,12 +8,19 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 //styling
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match }) => (
-  <div className="category">
-    <h2>Category Page</h2>
-    {/* <CollectionItem /> */}
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
