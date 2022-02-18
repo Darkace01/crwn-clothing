bb3cb9c2db53ffa225bf0c3852b239d6098e7b08
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-02-18 15:40:50 +0100

    Saving our shop data to firebase

--- src/App.js
+++ src/App.js
@@ -14,14 +14,19 @@ import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 
 //utils
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import {
+  auth,
+  createUserProfileDocument,
+  addCollectionAndDocuments,
+} from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
+import { selectCollectionsForPreview } from "./redux/shop/shop.selectors";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -34,11 +39,11 @@ class App extends React.Component {
           });
         });
       }
-      // console.log(userAuth);
-      // if (!userAuth) {
-      //   console.log("User is null");
-      // }
       setCurrentUser(userAuth);
+      addCollectionAndDocuments(
+        "collections",
+        collectionsArray.map(({ title, items }) => ({ title, items }))
+      );
     });
   }
   componentWillUnmount() {
@@ -71,6 +76,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview,
 });
 
 const mapDispatchTopProps = (dispatch) => ({
